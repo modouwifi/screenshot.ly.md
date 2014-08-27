9953aa5ba3bd254ffe89e9a977f2baf600a24901
Forrest Ye <afu@forresty.com>
2014-08-27 15:12:46 +0800

    try screenshot with ffmpeg

--- Gemfile
+++ Gemfile
@@ -4,3 +4,7 @@ ruby '2.0.0'
 
 gem 'sinatra'
 gem 'unicorn'
+
+group :development do
+  gem 'shotgun'
+end
--- app.rb
+++ app.rb
@@ -3,3 +3,13 @@ require "sinatra"
 get '/hello_world' do
   'hello world'
 end
+
+post '/screenshot' do
+  File.open(File.expand_path('../tmp/tmp.dat', __FILE__), 'wb') do |file|
+    file.write(params[:data][:tempfile].read)
+  end
+
+  `yes | ffmpeg -vcodec rawvideo -f rawvideo -pix_fmt rgb565 -s 320x240 -i tmp/tmp.dat -f image2 -vcodec png tmp/screenshot.png`
+
+  send_file 'tmp/screenshot.png'
+end
